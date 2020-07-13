f9f8d80ab47d1e04067d8a6c1f0a7b982bebf9f3
cristianlunachavarro <cristianlunachavarro@gmail.com>
2020-07-13 17:01:28 -0300

    Commiting to be able to make a pull

--- front/App.js
+++ front/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Test from './src/redux/Test'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Test/>
       <Text style={{color:"white"}}>EFETE</Text>
       <StatusBar style="auto" />
     </View>
