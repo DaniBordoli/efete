5493957c24b8a49bc9a086ae84802bddf0978252
joaquin gaona <joaquinngaona@gmail.com>
2020-08-06 17:13:37 -0300

    agentes mapa

--- back/seed/singleAgent.js
+++ back/seed/singleAgent.js
@@ -2,7 +2,7 @@ const Agent = require("../models/agents");
 require("../config/db");
 
 const charmander = {
-  name: "Kiosko",
+  name: "Kiosko1",
   address:
     "Coronel Lorenzo Barcala, Ituzaingó, Buenos Aires Province, Argentina",
   cuil: "273656928",
@@ -12,6 +12,28 @@ const charmander = {
   },
 };
 
-Agent.insertMany([charmander]).then(() => {
+const squirtle = {
+  name: "Kiosko2",
+  address:
+    "Coronel Lorenzo Barcala, Ituzaingó, Buenos Aires Province, Argentina",
+  cuil: "273656928",
+  ubicacion: { latitude: -34.64248919999999, longitude: -58.676052 },
+  user: {
+    _id: "5f2b221aa67f004137125dda",
+  },
+};
+
+const bulbasaur = {
+  name: "Kiosko3",
+  address:
+    "Coronel Lorenzo Barcala, Ituzaingó, Buenos Aires Province, Argentina",
+  cuil: "273656928",
+  ubicacion: { latitude: -34.64248919999999, longitude: -58.676052 },
+  user: {
+    _id: "5f2b221aa67f004137125dda",
+  },
+};
+
+Agent.insertMany([charmander, squirtle, bulbasaur]).then(() => {
   console.log("Ready to go!");
 });
